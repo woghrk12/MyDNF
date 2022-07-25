831d7b481b19bb2cd734af4431e52f24c79be515
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2022-07-25 21:47:02 +0900

    카메라 이동 범위 제한

--- Assets/2Script/Camera/CameraFollow.cs
+++ Assets/2Script/Camera/CameraFollow.cs
@@ -6,9 +6,29 @@ public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform target = null;
     [SerializeField] private Vector3 offset = Vector3.zero;
+    [SerializeField] private float minX = 0f, maxX = 0f;
+    [SerializeField] private float minY = 0f, maxY = 0f;
 
     private void LateUpdate()
     {
-        transform.position = target.transform.position + offset;
+        Follow(target);
+    }
+
+    private Vector3 LimitArea(Vector3 p_cameraPos)
+    {
+        Vector3 t_pos = p_cameraPos;
+
+        if (t_pos.x < minX) t_pos.x = minX;
+        if (t_pos.x > maxX) t_pos.x = maxX;
+        if (t_pos.y < minY) t_pos.y = minY;
+        if (t_pos.y > maxY) t_pos.y = maxY;
+
+        return t_pos;
+    }
+
+    private void Follow(Transform p_target)
+    {
+        Vector3 t_pos = p_target.transform.position + offset;
+        transform.position = LimitArea(t_pos);
     }
 }
