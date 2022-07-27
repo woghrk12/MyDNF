de5a06acdd4616320759ee3a0011f8ebbd5a159d
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2022-07-27 15:12:01 +0900

    스킬 사용 시 생성되는 Projectile 오브젝트 추가

---
+++ Assets/2Script/Skill/Projectile.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float damage = 0;
+    [SerializeField] private Vector2 range = Vector2.zero;
+}
