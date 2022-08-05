a921728f7dea110bfb1c720cb6b888d9655f5a49
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2022-08-05 13:48:22 +0900

    버튼의 상태를 반환하는 열거형 추가

--- Assets/2Script/Manager/Input/InputManager.cs
+++ Assets/2Script/Manager/Input/InputManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum EButtonState { IDLE, DOWN, PRESSED, UP }
+
 public class InputManager : MonoBehaviour
 {
     public Vector2 Direction { set { inputDir = value; } get { return inputDir; } }
@@ -15,4 +17,14 @@ public class InputManager : MonoBehaviour
     private bool xDown = false;
     private bool aDown = false;
     private bool sDown = false;
+
+    public EButtonState JButton { set { jButton = value; } get { return jButton; } }
+    public EButtonState XButton { set { xButton = value; } get { return xButton; } }
+    public EButtonState AButton { set { aButton = value; } get { return aButton; } }
+    public EButtonState SButton { set { sButton = value; } get { return sButton; } }
+
+    private EButtonState jButton = EButtonState.IDLE;
+    private EButtonState xButton = EButtonState.IDLE;
+    private EButtonState aButton = EButtonState.IDLE;
+    private EButtonState sButton = EButtonState.IDLE;
 }
