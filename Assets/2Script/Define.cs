63a6b7cb9458cf843cce10608cffd0d50a6d37e9
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2022-08-10 14:07:31 +0900

    스킬 인터페이스 스크립트 분류

--- Assets/2Script/Define.cs
+++ Assets/2Script/Define.cs
@@ -11,38 +11,3 @@ public struct Pool
     public GameObject prefab;
     public int size;
 }
-
-// Interface
-public interface ITargetingSkill
-{
-    public Vector3 Target { set; get; }
-    public void SetTarget();
-}
-
-public interface INonTargetingSkill
-{ 
-    public Vector3 Direction { set; get; }
-    public void SetDirection();
-}
-
-public interface IComboSkill
-{
-    public int NumOfClick { set; get; }
-    public IEnumerator CheckNumInput();
-}
-
-public interface IChargingSkill
-{
-    public IEnumerator CheckCharging();
-}
-
-public interface IProjectileSkill
-{ 
-    public string[] Projectile { set; get; }
-}
-
-public interface IHitboxSkill
-{ 
-    public HitBox[] HitBox { set; get; }
-}
-
