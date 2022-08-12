77bbfd19e024deceef647bcf0467999ccf03a159
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2022-08-12 16:07:08 +0900

    Skill 스크립트 이름 변경

---
+++ Assets/2Script/Skill/SkillD.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillD : Skill
+{
+    [SerializeField] private Charging chargingController = null;
+
+    public override IEnumerator UseSkill(Animator p_anim, bool p_isLeft, string p_button)
+    {
+        ApplyCoolTime();
+        yield return PreDelay(p_anim, 0);
+        yield return chargingController.CheckCharging(p_anim, p_button);
+        ActivateSkill(p_anim, p_isLeft, p_button, 0, chargingController.ChargingValue);
+        yield return PostDelay(p_anim, 0);
+    }
+
+    public override void ResetSkill(Animator p_anim)
+    {
+        base.ResetSkill(p_anim);
+        chargingController.ResetValue(p_anim);
+    }
+}
