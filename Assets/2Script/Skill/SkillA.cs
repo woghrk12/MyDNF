f2e94171efd8374bc2e72dfdfcf33fb9726406e8
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2022-08-25 15:42:39 +0900

    Single Skill 제거 및 Skill A 추가

--- Assets/2Script/Skill/SkillA.cs
+++ Assets/2Script/Skill/SkillA.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SingleSkill : Skill
+public class SkillA : Skill
 {
     public override IEnumerator UseSkill(Animator p_anim, bool p_isLeft, string p_button)
     {
@@ -11,5 +11,4 @@ public class SingleSkill : Skill
         ActivateSkill(p_anim, p_isLeft, p_button, 0);
         yield return PostDelay(p_anim, 0);
     }
-
 }
