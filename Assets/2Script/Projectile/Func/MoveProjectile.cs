033f0e3192e36cbced96676aa53f4a1e04d2b70d
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2022-08-26 12:58:32 +0900

    투사체 초기 이동 속도 및 방향 설정 기능 이전

--- Assets/2Script/Projectile/Func/MoveProjectile.cs
+++ Assets/2Script/Projectile/Func/MoveProjectile.cs
@@ -6,25 +6,26 @@ public class MoveProjectile : MonoBehaviour
 {
     [SerializeField] private Vector3 rawDirection = Vector3.zero;
     [SerializeField] private bool boostFlag = false;
-    [SerializeField] private float startSpeed = 0f;
     private Vector3 direction = Vector3.zero;
 
-    public void SetDirection(bool p_isLeft)
+    public void SetDirection(bool p_isLeft, Vector3? p_direction = null)
     {
-        direction = new Vector3(p_isLeft ? -rawDirection.x : rawDirection.x, rawDirection.y, rawDirection.z).normalized;
+        direction = p_direction.HasValue
+            ? direction = p_direction.Value.normalized
+            : new Vector3(p_isLeft ? -rawDirection.x : rawDirection.x, rawDirection.y, rawDirection.z).normalized;
     }
 
-    public void Move(float p_duration) => StartCoroutine(MoveCo(p_duration));
+    public void Move(float p_duration, float p_startSpeed) => StartCoroutine(MoveCo(p_duration, p_startSpeed));
 
-    private IEnumerator MoveCo(float p_duration)
+    private IEnumerator MoveCo(float p_duration, float p_startSpeed)
     {
         var t_timer = 0f;
-        var t_speed = boostFlag ? startSpeed : 0f;
+        var t_speed = boostFlag ? p_startSpeed : 0f;
         while (t_timer < p_duration)
         {
             t_speed = boostFlag
-                ? Mathf.Lerp(startSpeed, 0f, t_timer / p_duration)
-                : Mathf.Lerp(0f, startSpeed, t_timer / p_duration);
+                ? Mathf.Lerp(p_startSpeed, 0f, t_timer / p_duration)
+                : Mathf.Lerp(0f, p_startSpeed, t_timer / p_duration);
             transform.position += direction * t_speed * Time.deltaTime;
             t_timer += Time.deltaTime;
             yield return null;
