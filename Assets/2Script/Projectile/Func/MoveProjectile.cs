6c2409f396ecf8c6bc9a88d982680b37a7fd92d9
woghrk12 <72853328+woghrk12@users.noreply.github.com>
2022-08-30 14:14:31 +0900

    투사체 이동 방향, 속도 및 플래그 인수화

--- Assets/2Script/Projectile/Func/MoveProjectile.cs
+++ Assets/2Script/Projectile/Func/MoveProjectile.cs
@@ -4,42 +4,33 @@ using UnityEngine;
 
 public class MoveProjectile : MonoBehaviour
 {
-    [SerializeField] private Vector3 rawDirection = Vector3.zero;
-    [SerializeField] private bool boostFlag = false;
-    private Vector3 direction = Vector3.zero;
+    public IEnumerator LerpMove(HitBox p_hitBox, float p_duration, float p_startSpeed, Vector3 p_direction, bool p_boostFlag) 
+        => LerpMoveCo(p_hitBox, p_duration, p_startSpeed, p_direction, p_boostFlag);
+    public IEnumerator ConstantMove(HitBox p_hitBox, float p_duration, float p_startSpeed, Vector3 p_direction) 
+        => ConstantMoveCo(p_hitBox, p_duration, p_startSpeed, p_direction);
 
-    public void SetDirection(bool p_isLeft, Vector3? p_direction = null)
-    {
-        direction = p_direction.HasValue
-            ? direction = p_direction.Value.normalized
-            : new Vector3(p_isLeft ? -rawDirection.x : rawDirection.x, rawDirection.y, rawDirection.z).normalized;
-    }
-
-    public void LerpMove(float p_duration, float p_startSpeed) => StartCoroutine(LerpMoveCo(p_duration, p_startSpeed));
-    public void ConstantMove(float p_duration, float p_startSpeed) => StartCoroutine(ConstantMoveCo(p_duration, p_startSpeed));
-
-    private IEnumerator LerpMoveCo(float p_duration, float p_startSpeed)
+    private IEnumerator LerpMoveCo(HitBox p_hitBox, float p_duration, float p_startSpeed, Vector3 p_direction, bool p_boostFlag)
     {
         var t_timer = 0f;
-        var t_speed = boostFlag ? p_startSpeed : 0f;
+        var t_speed = p_boostFlag ? p_startSpeed : 0f;
         while (t_timer < p_duration)
         {
-            t_speed = boostFlag
+            t_speed = p_boostFlag
                 ? Mathf.Lerp(p_startSpeed, 0f, t_timer / p_duration)
                 : Mathf.Lerp(0f, p_startSpeed, t_timer / p_duration);
-            transform.position += direction * t_speed * Time.deltaTime;
+            p_hitBox.ObjectPos += p_direction * t_speed * Time.deltaTime;
             t_timer += Time.deltaTime;
             yield return null;
         }
     }
 
-    private IEnumerator ConstantMoveCo(float p_duration, float p_startSpeed)
+    private IEnumerator ConstantMoveCo(HitBox p_hitBox, float p_duration, float p_startSpeed, Vector3 p_direction)
     {
         var t_timer = 0f;
         
         while (t_timer < p_duration)
         {
-            transform.position += direction * p_startSpeed * Time.deltaTime;
+            p_hitBox.ObjectPos += p_direction * p_startSpeed * Time.deltaTime;
             t_timer += Time.deltaTime;
             yield return null;
         }
